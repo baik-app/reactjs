699985d487fdec6e6ecb7a3d12d4f18e41069421
fajarnu <fajarnu1608@gmail.com>
2021-10-03 18:54:16 +0700

    ngebenerin function call

--- src/pages/Components/Button-Panggil/PanggilOn.js
+++ src/pages/Components/Button-Panggil/PanggilOn.js
@@ -2,15 +2,14 @@ import React from 'react'
 import './style.css'
 
 function PanggilOn(callNumber) {
-    function call(number){
-        return "tel:" + number
+    function call(){
+        return callNumber
     }
     return (
         <div className='container-panggil'>
-            <a href={call(callNumber)}><div className='button-panggil-on'><p>Panggil</p></div></a>
+            <a href={'tel:'+call()}><div className='button-panggil-on'><p>Panggil</p></div></a>
         </div>
     )
-    console.log(call());
 }
 
 export default PanggilOn
--- src/pages/NoDarurat.js
+++ src/pages/NoDarurat.js
@@ -5,6 +5,7 @@ import Box from './Components/Box-Nomor/BoxNomor'
 import './style.css'
 import Close from './Assets/close.svg'
 import PanggilOff from './Components/Button-Panggil/PanggilOff'
+import './Components/Button-Panggil/PanggilOn'
 import PanggilOn from './Components/Button-Panggil/PanggilOn'
 
 
@@ -17,14 +18,14 @@ function NoDarurat() {
             <div className="judul-layanan"><p>Silahkan pilih nomor yang akan dihubungi</p></div>
             <div>
                 <ul>
-                    <li onClick={()=> setPanggil(<PanggilOn callNumber='119' />)}><Box number="119 - Ambulance/Kemenkes"/></li>
-                    <li onClick={()=> setPanggil(<PanggilOn callNumber='110' />)}><Box number="110 - Polisi"/></li>
-                    <li onClick={()=> setPanggil(<PanggilOn callNumber='113' />)}><Box number="113 - Pemadam Kebakaran"/></li>
-                    <li onClick={()=> setPanggil(<PanggilOn callNumber='115' />)}><Box number="115 - Search And Rescue"/></li>
-                    <li onClick={()=> setPanggil(<PanggilOn callNumber='122' />)}><Box number="122 - Posko Kewaspadaan Nasional"/></li>
-                    <li onClick={()=> setPanggil(<PanggilOn callNumber='115' />)}><Box number="115 - PLN"/></li>
-                    <li onClick={()=> setPanggil(<PanggilOn callNumber='113' />)}><Box number="113 - Hotline Covid-19"/></li>
-                    <li onClick={()=> setPanggil(<PanggilOn callNumber='021799232' />)}><Box number="021799325 - PMI"/></li>
+                    <li onClick={()=> setPanggil(PanggilOn('119'))}><Box number="119 - Ambulance/Kemenkes"/></li>
+                    <li onClick={()=> setPanggil(PanggilOn('110'))}><Box number="110 - Polisi"/></li>
+                    <li onClick={()=> setPanggil(PanggilOn('113'))}><Box number="113 - Pemadam Kebakaran"/></li>
+                    <li onClick={()=> setPanggil(PanggilOn('115'))}><Box number="115 - Search And Rescue"/></li>
+                    <li onClick={()=> setPanggil(PanggilOn('122'))}><Box number="122 - Posko Kewaspadaan Nasional"/></li>
+                    <li onClick={()=> setPanggil(PanggilOn('115'))}><Box number="115 - PLN"/></li>
+                    <li onClick={()=> setPanggil(PanggilOn('113'))}><Box number="113 - Hotline Covid-19"/></li>
+                    <li onClick={()=> setPanggil(PanggilOn('021799325'))}><Box number="021799325 - PMI"/></li>
                 </ul>
             </div>
             <div><p>{panggil}</p></div>
