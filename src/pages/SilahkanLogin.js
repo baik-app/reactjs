dde32e6e461ba943acc99410d9afe607f6a22ef9
ridwans15 <ridwan.sophian.150304@gmail.com>
2021-10-05 11:58:35 +0700

    commit ridwan

---
+++ src/pages/SilahkanLogin.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './style.css'
+import close from './Assets/close.svg'
+import {Link} from 'react-router-dom'
+
+function SilahkanLogin() {
+    return (
+        <div className="container-silahkanLogin">
+            <Link to='/opsi'><img className="close-login" src={close} alt='close'/></Link>
+            <div className="header-login">
+                <h1>DAFTAR KE BAIK APP</h1>
+                <p>Silahkan daftar untuk melanjutkan pelaporan</p>
+            </div>
+            <div >
+                <Link to='/register'>
+                <button id="button-daftar" type="button" ><p>DAFTAR</p></button>
+                </Link>
+            </div>
+            <div className="masuk-login">
+                <p>Sudah punya akun?</p>
+                <Link to='/login' style={{ color: 'red', textDecoration: 'inherit'}}><p> Masuk</p></Link>
+            </div>
+            <div className="ketentuan"><p>Dengan masuk atau mendaftar, saya menyetujui Ketentuan Layanan dan Kebijakan Privasi kami</p></div>
+        </div>
+
+    )
+}
+
+export default SilahkanLogin
