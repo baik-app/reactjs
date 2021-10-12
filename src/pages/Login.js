c9cb693efc0d1babb66031399c006b24afc8fb01
Riskief <Riskiefendi227@gmail.com>
2021-10-12 13:20:42 +0700

    nambah style login regis

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,80 +1,100 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import "./style.css";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./Components/loginregis/styles.css";
 import Next from "./Assets/next.svg";
+import Back from "./Assets/back.svg";
 
 function Login() {
- 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [isLogin, setIsLogin] = useState(0)
- 
-    useEffect(() => {
-        if (localStorage.getItem('isLogin') === '1') {
-            setIsLogin(1)
-        }
-    }, [])
- 
-    const onSubmitHandler = (e) => {
-        const getDataLc = JSON.parse(localStorage.getItem('users'))
-        console.log(getDataLc)
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+   const [isLogin, setIsLogin] = useState(0);
 
-        let match
+   useEffect(() => {
+      if (localStorage.getItem("isLogin") === "1") {
+         setIsLogin(1);
+      }
+   }, []);
 
-        if (getDataLc) {
-            for (let i = 0; i < getDataLc.length; i++) {
-                if (getDataLc[i].email === email && getDataLc[i].password === password) {
-                    localStorage.setItem('isLogin', 1)
-                    localStorage.setItem('email', email)
-                    setIsLogin(1)
-                    match = true
-                    break
-                } else {
-                    match = false
-                }
+   const onSubmitHandler = (e) => {
+      const getDataLc = JSON.parse(localStorage.getItem("users"));
+      console.log(getDataLc);
+
+      let match;
+
+      if (getDataLc) {
+         for (let i = 0; i < getDataLc.length; i++) {
+            if (getDataLc[i].email === email && getDataLc[i].password === password) {
+               localStorage.setItem("isLogin", 1);
+               localStorage.setItem("email", email);
+               setIsLogin(1);
+               match = true;
+               break;
+            } else {
+               match = false;
             }
-            if (!match) {
-                alert('Email atau password salah')
-            }
-        } else {
-            alert('Email atau password tidak ditemukan')
-        }
-        
-        e.preventDefault()
-    }
- 
-    return (
-        <div>
-        {isLogin === 1 ? 
+         }
+         if (!match) {
+            alert("Email atau password salah");
+         }
+      } else {
+         alert("Email atau password tidak ditemukan");
+      }
+
+      e.preventDefault();
+   };
+
+   return (
+      <div>
+         {isLogin === 1 ? (
             <div className="container-selamatdatang">
-            <p>Selamat datang kembali di Baik App. Laporkan segala bentuk kejahatan disini.</p>
-            <Link to="/Pelaporan">
-            <img className="next" src={Next} alt="next" />
-            </Link>
-            </div>:
-            
-        <div className="page-login">
-            <h1>Login</h1>
-            <label htmlFor="email">Email: </label>
-            <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
- 
-            <br />
-            <br />
- 
-            <label htmlFor="password">Password : </label>
-            <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
- 
-            <br />
-            <br />
- 
-            <button type="submit" onClick={(e) => onSubmitHandler(e)}>Masuk</button>
-            <p>Belum punya akun?</p> 
-            <Link to="/Register"
-            style={{ color: "red", textDecoration: "inherit" }}> <p> Daftar</p>
-            </Link>
-        </div>}
-        </div>
-    )
+               <div className="welcomes">
+                  <p>Selamat datang di Baik App. Laporkan segala bentuk kejahatan disini.</p>
+                  <Link to="/Pelaporan">
+                     <img className="next" src={Next} alt="next" />
+                  </Link>
+               </div>
+            </div>
+         ) : (
+            <div className="login-form">
+               <div className="judul-login">
+                  <h2>Masuk Ke Baik App</h2>
+                  <Link to="/SilahkanLogin">
+                     <img src={Back} alt="back" />
+                  </Link>
+               </div>
+
+               <div className="conten-login">
+                  <div className="emailLog">
+                     <label htmlFor="judulLog">Email: </label>
+                     <br />
+                     <input type="email" placeholder="Masukan email anda" className="form-judulLog" value={email} onChange={(e) => setEmail(e.target.value)} />
+                  </div>
+
+                  <div className="passwordLog">
+                     <label htmlFor="judulLog">Password : </label>
+                     <br />
+                     <input type="password" placeholder="Masukan password email anda" className="form-judulLog" value={password} onChange={(e) => setPassword(e.target.value)} />
+                  </div>
+                  <br />
+               </div>
+
+               {/* <div className="masukLog"> */}
+               <button className="masukLog" type="submit" onClick={(e) => onSubmitHandler(e)}>
+                  <p>Masuk</p>
+               </button>
+               {/* </div> */}
+
+               <div className="keDaftar">
+                  <p>Belum punya akun? </p>
+                  <Link to="/Register" style={{ color: "red", textDecoration: "inherit" }}>
+                     {" "}
+                     <p> Daftar</p>
+                  </Link>
+               </div>
+            </div>
+         )}
+      </div>
+   );
 }
- 
-export default Login
+
+export default Login;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,95 +1,111 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import Masuk from './Login'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Components/loginregis/styles.css";
+import Masuk from "./Login";
+import Back from "./Assets/back.svg";
 
 function Register() {
+   let getLC;
 
-    let getLC
+   const [nik, setNik] = useState("");
+   const [nama, setNama] = useState("");
+   const [tempattinggal, setTempat] = useState("");
+   const [tanggallahir, setTanggal] = useState("");
+   const [jeniskelamin, setJenisKelamin] = useState("");
+   const [notelpon, setNotelpon] = useState("");
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+   const [konfirmasi, setKonfirPassword] = useState("");
 
-    const [nik, setNik] = useState('')
-    const [nama, setNama] = useState('')
-    const [tempattinggal, setTempat] = useState('')
-    const [tanggallahir, setTanggal] = useState('')
-    const [jeniskelamin, setJenisKelamin] = useState('')
-    const [notelpon, setNotelpon] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [konfirmasi, setKonfirPassword] = useState('')
+   const onRegister = (e) => {
+      console.log("register ditekan");
+      getLC = {
+         nik,
+         nama,
+         tempattinggal,
+         tanggallahir,
+         jeniskelamin,
+         notelpon,
+         email,
+         password,
+      };
+      const getLcData = localStorage.getItem("users") ? JSON.parse(localStorage.getItem("users")) : [];
+      console.log(getLcData);
 
-    const onRegister = (e) => {
-        console.log('register ditekan')
-        getLC = {
-            nik, nama, tempattinggal, tanggallahir, jeniskelamin, notelpon, email,
-            password
-        }
-        const getLcData = localStorage.getItem('users') ? JSON.parse(localStorage.getItem('users')) : []
-        console.log(getLcData)
+      getLcData.push(getLC);
+      console.log(getLcData);
 
-        getLcData.push(getLC)
-        console.log(getLcData)
+      localStorage.setItem("users", JSON.stringify(getLcData));
+      console.log("register berhasil!");
 
-        localStorage.setItem('users', JSON.stringify(getLcData))
-        console.log('register berhasil!')
-        
-        e.preventDefault()
-    }
+      e.preventDefault();
+   };
 
-    return (
-        <div className="page-register">
-            <h1>Daftar Ke Baik App</h1>
-            <div className="nik">
-            <label htmlFor="nik">NIK*  </label>
-            <input type="text" id="nik" value={nik} onChange={(e) => setNik(e.target.value)} />
-            </div>
-
-            <div className="nama-lengkap">
-            <label htmlFor="username">Nama lengkap*  </label>
-            <input type="text" id="nama" value={nama} onChange={(e) => setNama(e.target.value)} />
-            </div>
-
-            <div className="tempat-tinggal">
-            <label htmlFor="tempat-tinggal">Tempat tinggal saat ini* </label>
-            <input type="text" id="tempat-tinggal" value={tempattinggal} onChange={(e) => setTempat(e.target.value)} />
-            </div>
-
-            <div className="tanggal-lahir">
-            <label htmlFor="tanggal-lahir">Tanggal lahir* </label>
-            <input type="date" id="tanggal-lahir" value={tanggallahir} onChange={(e) => setTanggal(e.target.value)} />
-            </div>
-
-            <div className="jenis-kelamin">
-            <label htmlFor="jenis-kelamin">Jenis kelamin* </label>
-            <input type="text" id="jeniskelamin" value={jeniskelamin} onChange={(e) => setJenisKelamin(e.target.value)} /> 
-            </div>
-
-            <div className="no-telpon">
-            <label htmlFor="no-telpon">No. Telp aktif* </label>
-            <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" value={notelpon} onChange={(e) => setNotelpon(e.target.value)} /> 
-            </div>
-
-            <div className="email">
-            <label htmlFor="email">Alamat email* </label>
-            <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} /> 
-            </div>
-
-            <div className="password">
-            <label htmlFor="password">Password* </label>
-            <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </div>
-
-            <div className="konfirmasi-password">
-            <label htmlFor="konfirmasi-password">Konfirmasi Password* </label>
-            <input type="password" id="password" value={konfirmasi} onChange={(e) => setKonfirPassword(e.target.value)} />
-            </div>
-            
-
-            <button type="submit" onClick={(e) => onRegister(e)}>Register </button>
-            <p>Sudah punya akun?</p> 
-            <Link to="/Login"
-            style={{ color: "red", textDecoration: "inherit" }}> <p>Masuk</p>
+   return (
+      <div className="container-register">
+         <div className="judul-login">
+            <h2>Daftar Ke Baik App</h2>
+            <Link to="/SilahkanLogin">
+               <img src={Back} alt="back" />
             </Link>
-        </div>
-    )
+         </div>
+         <div className="nik">
+            <label htmlFor="judulLog">NIK* </label>
+            <br />
+            <input type="text" id="form-judulLog" value={nik} onChange={(e) => setNik(e.target.value)} />
+         </div>
+         <div className="nama-lengkap">
+            <label htmlFor="nama-lengkap">Nama lengkap* </label>
+            <br />
+            <input type="text" id="form-judulLog" value={nama} onChange={(e) => setNama(e.target.value)} />
+         </div>
+         <div className="tempat-tinggal">
+            <label htmlFor="tempat-tinggal">Tempat tinggal saat ini* </label>
+            <br />
+            <input type="text" id="form-judulLog" value={tempattinggal} onChange={(e) => setTempat(e.target.value)} />
+         </div>
+         <div className="tanggal-lahir">
+            <label htmlFor="tanggal-lahir">Tanggal lahir* </label>
+            <br />
+            <input type="date" id="form-judulLog" value={tanggallahir} onChange={(e) => setTanggal(e.target.value)} />
+         </div>
+         <div className="jenis-kelamin">
+            <label htmlFor="jenis-kelamin">Jenis kelamin (L/P)* </label>
+            <br />
+            <input type="text" id="form-judulLog" value={jeniskelamin} onChange={(e) => setJenisKelamin(e.target.value)} />
+         </div>
+         <div className="no-telpon">
+            <label htmlFor="no-telpon">No. Telp aktif* </label>
+            <br />
+            <input type="tel" id="form-judulLog" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" value={notelpon} onChange={(e) => setNotelpon(e.target.value)} />
+         </div>
+         <div className="emailReg">
+            <label htmlFor="emailReg">Alamat email* </label>
+            <br />
+            <input type="email" id="form-judulLog" value={email} onChange={(e) => setEmail(e.target.value)} />
+         </div>
+         <div className="passwordReg">
+            <label htmlFor="passwordReg">Password* </label>
+            <br />
+            <input type="password" id="form-judulLog" value={password} onChange={(e) => setPassword(e.target.value)} />
+         </div>
+         <div className="konfirmasi-password">
+            <label htmlFor="konfirmasi-password">Konfirmasi Password* </label>
+            <br />
+            <input type="password" id="form-judulLog" value={konfirmasi} onChange={(e) => setKonfirPassword(e.target.value)} />
+         </div>
+         <button className="masukReg" type="submit" onClick={(e) => onRegister(e)}>
+            <p>Register</p>
+         </button>
+         <div className="keLogin">
+            <p>Sudah punya akun?</p>
+            <Link to="/Login" style={{ color: "red", textDecoration: "inherit" }}>
+               {" "}
+               <p>Masuk</p>
+            </Link>{" "}
+         </div>
+      </div>
+   );
 }
 
-export default Register
+export default Register;
